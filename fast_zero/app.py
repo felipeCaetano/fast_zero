c7641a512d4c0e5cdbc2788a8e7f1dbf9b5fea7d
felipeCaetano <felipecmelo@gmail.com>
2024-06-30 05:58:03 -0300

    aula 3 - 1:17:49

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,26 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
-from fast_zero.schemas import Message
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
 app = FastAPI()
+
+database = []
 
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá, mundo!"}
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get("/users/", response_model=UserList)
+def read_users():
+    return {"users": database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,38 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá, mundo!"}
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "testusername",
+            "password": "password",
+            "email": "test@test.com",
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "testusername",
+        "email": "test@test.com",
+        "id": 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get("/users/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "testusername",
+                "email": "test@test.com",
+                "id": 1,
+            }
+        ],
+    }
