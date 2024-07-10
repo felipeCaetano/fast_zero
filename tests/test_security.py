9e643c9bcdb1cc25a45b454f9b877446c3bcc858
felipeCaetano <felipecmelo@gmail.com>
2024-07-10 05:05:45 -0300

    aula 7 - refatorando a estrutura do projeto

--- tests/test_security.py
+++ tests/test_security.py
@@ -2,19 +2,21 @@ from http import HTTPStatus
 
 from jwt import decode
 
-from fast_zero.security import ALGORITHM, SECRET_KEY, create_access_token
+from fast_zero.security import create_access_token, settings
 
 
 def test_jwt():
     data = {"sub": "test@test.com"}
     token = create_access_token(data)
-    result = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    result = decode(
+        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
     assert result["sub"] == data["sub"]
     assert result["exp"]
 
 
 def test_jwt_invalid_token(client):
-    response = client.delete(
+    response = client.delete_user(
         "users/1", headers={"Autorization": "Bearer token-invalido"}
     )
     assert response.status_code == HTTPStatus.UNAUTHORIZED
---
+++ tests/test_users.py
@@ -0,0 +1,89 @@
+from http import HTTPStatus
+
+from fast_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "testusername",
+            "password": "password",
+            "email": "test@test.com",
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "testusername",
+        "email": "test@test.com",
+        "id": 1,
+    }
+
+
+def test_read_users(client, token):
+    response = client.get(
+        "/users/", headers={"Autorization": f"Bearer {token}"}
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "Test",
+                "email": "test@test.com",
+                "id": 1,
+            }
+        ]
+    }
+
+
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get("/users/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"users": [user_schema]}
+
+
+def test_update_user(client, user, token):
+    response = client.put(
+        f"/users/{user.id}",
+        headers={"Autorization": f"Bearer {token}"},
+        json={
+            "username": "testuser",
+            "email": "test@test.com",
+            "password": "123",
+            "id": user.id,
+        },
+    )
+    assert response.json() == {
+        "username": "testuser",
+        "email": "test@test.com",
+        "id": user.id,
+    }
+
+
+def test_update_user_retorna_404(client):
+    response = client.put(
+        "/users/2",
+        json={
+            "username": "testuser",
+            "email": "test@test.com",
+            "password": "123",
+            "id": 1,
+        },
+    )
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f"/users/{user.id}", headers={"Autorization": f"Bearer {token}"}
+    )
+    assert response.json() == {"message": "User deleted."}
+
+
+def test_delete_user_retorna_404(client, user, token):
+    response = client.delete(
+        f"/users/{user.id + 1}", headers={"Autorization": f"Bearer {token}"}
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {"detail": "Not enough permissions."}
